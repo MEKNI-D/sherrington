6f00ee69de1ab4e943fd3b8a435078c27586a606
Donia <mekni.donia92@gmail.com>
2016-10-26 00:20:57 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -3,5 +3,5 @@
  */
 var mongoose=require('mongoose');
 //mongoose.connect('mongodb://firstak:firstak2016@ds040349.mlab.com:40349/sherrington');
-mongoose.connect('mongodb://127.0.0.1/meanAuth');
+mongoose.connect('mongodb://admin:azerty100@ds031597.mlab.com:31597/sherrington');
 module.exports=mongoose;
